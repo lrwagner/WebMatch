126c139fabba2d73fcf1c433e683c30376ba5b5a
lrwagner <louisr.wagner@gmail.com>
2020-03-21 13:52:27 +0100

    added defaults in models, refactoring

--- app/models.py
+++ app/models.py
@@ -3,15 +3,15 @@ from django.db import models
 class Person(models.Model):
     char_length = 255
 
-    name = models.CharField(max_length= char_length)
-    surname = models.CharField(max_length= char_length)
-    date_of_birth = models.DateField()
+    name = models.CharField(max_length=char_length, default=None)
+    surname = models.CharField(max_length=char_length, default=None)
+    date_of_birth = models.DateField(default=None)
 
-    adress = models.CharField(max_length= char_length)
-    house_number = models.PositiveIntegerField()
-    zip_code = models.PositiveIntegerField()
+    adress = models.CharField(max_length=char_length, default=None)
+    house_number = models.PositiveIntegerField(default=None)
+    zip_code = models.PositiveIntegerField(default=None)
 
-    last_access = models.TimeField() # saves as in datetime.date instance
+    last_access = models.TimeField(default=None) # saves as in datetime.date instance
 
 
 
