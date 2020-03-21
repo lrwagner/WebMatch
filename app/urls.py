5ea1ceaffb96078b47bd311b49121ab89ca8e35e
lrwagner <louisr.wagner@gmail.com>
2020-03-21 17:33:46 +0100

    createuser

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 from app import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('usercreation', views.UserCreationView.as_view(), name='usercreation')
 ]
