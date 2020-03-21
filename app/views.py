af61944772cf0ccad146625ab0b972caec9c7b96
lrwagner <louisr.wagner@gmail.com>
2020-03-21 14:07:00 +0100

    Anzeige für alle User auf Index.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from app import models
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'app/index.html'
+
+    def get_queryset(self):
+        object_list = models.Person.objects.all()
+        return object_list
+
+    
+    
