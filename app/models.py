653ee09342b48520c7be1e1ce4b8577c40b1a511
David Thinnes <thinnes@snn-unit.de>
2020-03-21 12:49:02 +0100

    models erweitert

--- app/models.py
+++ app/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 class Person(models.Model):
-    name = models.CharField(max_length=255)
-    age = models.IntegerField()
+    char_length = 255
+
+    name = models.CharField(max_length= char_length)
+    surname = models.CharField(max_length= char_length)
+    date_of_birth = models.DateField()
+
+    adress = models.CharField(max_length= char_length)
+    house_number = models.PositiveIntegerField()
+    zip_code = models.PositiveIntegerField()
+
+    last_access = models.TimeField() # saves as in datetime.date instance
+
+
+
     
 
+
 # Create your models here.
-    
