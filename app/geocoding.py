b5a4bfdf6774561220d3bdaa22dc829deba5f016
lrwagner <louisr.wagner@gmail.com>
2020-03-21 15:28:41 +0100

    update geocoding

--- app/geocoding.py
+++ app/geocoding.py
@@ -3,7 +3,10 @@ import requests
 
 def get_gps(address):
     # Set up your Geocoding url
-    geocode_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}"
+    with open('api/apikey', 'r') as key:
+        api_key = key.read()
+    print(api_key)
+    geocode_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={api_key}"
 
         
     # Ping google for the reuslts:
@@ -12,5 +15,5 @@ def get_gps(address):
     return results.json()
 
 
-q = get_gps(address='18 Grafton Street, Dublin, Ireland')
+q = get_gps(address='Brauerstraße 17, Saarbrücken, Germany')
 print(q)
