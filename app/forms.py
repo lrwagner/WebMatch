37dc7d3eaabcf30991eef6fc7898117511f29144
lrwagner <louisr.wagner@gmail.com>
2020-03-22 11:45:44 +0100

    adjust form to use ModelForm with all fields

--- app/forms.py
+++ app/forms.py
@@ -4,15 +4,7 @@ from .models import Person
 
 class PersonForm(forms.ModelForm):
 
+
     class Meta:
         model = Person
-        fields = [
-            'name',
-            'surname',
-            'date_of_birth',
-            'country',
-            'city',
-            'street',
-            'house_number',
-            'zip_code'
-        ]
+        fields = '__all__'
