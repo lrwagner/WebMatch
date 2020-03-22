d3afb15b3b4e6fb09fd1386723b343d3e94cde13
lrwagner <louisr.wagner@gmail.com>
2020-03-22 11:46:35 +0100

    added TODOs to models

--- app/models.py
+++ app/models.py
@@ -4,11 +4,12 @@ CHAR_L = 255
 
 class Person(models.Model):
     
-
+    # TODO name als Primary Key, Form für User Creation, Passwort?
     name = models.CharField(max_length=CHAR_L, default=None)
     surname = models.CharField(max_length=CHAR_L, default=None)
     date_of_birth = models.DateField(default=None)
 
+    # TODO evtl als eigenes Model mit eigener Form, Schritt 2 nach User Creation
     country = models.CharField(max_length=CHAR_L, default=None)
     city = models.CharField(max_length=CHAR_L, default=None)
     street = models.CharField(max_length=CHAR_L, default=None)
@@ -17,6 +18,7 @@ class Person(models.Model):
 
 
 class Location(models.Model):
+    # TODO Foreign Field Verbindung zu Person
     locationGPS = models.CharField(max_length=CHAR_L, default=None)
 
 
