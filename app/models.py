6b740093378b02b6e70a226dadf0ddd7f09c793c
David Thinnes <thinnes@snn-unit.de>
2020-03-22 19:32:42 +0100

    prim/foreign-key relationship implemented

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+import uuid # import universally unique identifier (ranodm number generation 128 bit)
 
 CHAR_L = 255
 
 class Person(models.Model):
     
     # TODO name/alternativ Username einführen als Primary Key, Form für User Creation, Passwort?
+    user_name = models.CharField(verbose_name='User Name', max_length=CHAR_L, primary_key=True, default= uuid4)
     name = models.CharField(verbose_name='First Name', max_length=CHAR_L, default=None)
     surname = models.CharField(verbose_name='Last Name', max_length=CHAR_L, default=None)
     date_of_birth = models.DateField(verbose_name='Date of Birth', default=None)
@@ -15,11 +17,20 @@ class Person(models.Model):
     street = models.CharField(max_length=CHAR_L, default=None)
     house_number = models.PositiveIntegerField(verbose_name='House Number', default=None)
     zip_code = models.PositiveIntegerField(verbose_name='Zip Code', default=None)
-
+  
+     def save(self, *args, **kwargs):
+        super(Person, self).save(*args, **kwargs)
+        if self.pk is None:
+            location = Location()
+            location.uuid = self
+            location.save()
 
 class Location(models.Model):
     # TODO Foreign Field Verbindung zu Person
+    user_name = models.OneToOneField(verbose_name= 'User Name', primary_key=True)
     locationGPS = models.CharField(max_length=CHAR_L, default=None)
+
+
 
 
 class TimeLogs(models.Model):
