a131038009c852c6847752bb85df8c4a696d407e
lrwagner <louisr.wagner@gmail.com>
2020-03-22 14:44:36 +0100

    updated models with verbose names

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,10 @@ from .models import Person
 
 class PersonForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # remove colon as label prefix in .html
+        self.label_suffix = ""
 
     class Meta:
         model = Person
--- app/models.py
+++ app/models.py
@@ -5,16 +5,16 @@ CHAR_L = 255
 class Person(models.Model):
     
     # TODO name/alternativ Username einführen als Primary Key, Form für User Creation, Passwort?
-    name = models.CharField(max_length=CHAR_L, default=None)
-    surname = models.CharField(max_length=CHAR_L, default=None)
-    date_of_birth = models.DateField(default=None)
+    name = models.CharField(verbose_name='First Name', max_length=CHAR_L, default=None)
+    surname = models.CharField(verbose_name='Last Name', max_length=CHAR_L, default=None)
+    date_of_birth = models.DateField(verbose_name='Date of Birth', default=None)
 
     # TODO evtl als eigenes Model mit eigener Form, Schritt 2 nach User Creation
     country = models.CharField(max_length=CHAR_L, default=None)
     city = models.CharField(max_length=CHAR_L, default=None)
     street = models.CharField(max_length=CHAR_L, default=None)
-    house_number = models.PositiveIntegerField(default=None)
-    zip_code = models.PositiveIntegerField(default=None)
+    house_number = models.PositiveIntegerField(verbose_name='House Number', default=None)
+    zip_code = models.PositiveIntegerField(verbose_name='Zip Code', default=None)
 
 
 class Location(models.Model):
